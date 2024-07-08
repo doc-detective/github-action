60359d9fabec01cc11e1ef3836da14b529bdf478
hawkeyexl <hawkeyexl@gmail.com>
2024-07-08 14:15:41 -0700

    Made async

--- dist/index.js
+++ dist/index.js
@@ -32328,54 +32328,58 @@ const core = __nccwpck_require__(2186);
 const { exec } = __nccwpck_require__(1514);
 const github = __nccwpck_require__(5438);
 
-try {
-  // Get the inputs
-  const version = core.getInput("version");
-  const dd = `doc-detective@${version}`;
-  const command = core.getInput("command");
-  const config = core.getInput("config");
-  const input = core.getInput("input");
-  const output = core.getInput("output");
+process.env["DOC_DETECTIVE_META"] = JSON.stringify({dist_interface: "github-actions"});
+main();
 
-  // Compile command
-  let compiledCommand = `npx ${dd} ${command}`;
-  if (config) compiledCommand += ` --config ${config}`;
-  if (input) compiledCommand += ` --input ${input}`;
-  if (output) compiledCommand += ` --output ${output}`;
+async function main() {
+  try {
+    // Get the inputs
+    const version = core.getInput("version");
+    const dd = `doc-detective@${version}`;
+    const command = core.getInput("command");
+    const config = core.getInput("config");
+    const input = core.getInput("input");
+    const output = core.getInput("output");
 
-  // Run Doc Detective
-  core.info(`Running Doc Detective: ${compiledCommand}`);
-  let commandOutputData = "";
-  const options = {};   // Full options: https://github.com/actions/toolkit/blob/d9347d4ab99fd507c0b9104b2cf79fb44fcc827d/packages/exec/src/interfaces.ts#L5
-  options.listeners = {
-    stdout: (data) => {
-      commandOutputData += data.toString();
-    },
-    stdline: (data) => {
-      commandOutputData += data.toString();
-    },
-    stderr: (data) => {
-      commandOutputData += data.toString();
-    },
-    errline: (data) => {
-      commandOutputData += data.toString();
-    },
-    debug: (data) => {
-      commandOutputData += data.toString();
-    },
-  };
-  const commandOutput = exec(compiledCommand, options);
-  console.log("Command output:");
-  console.log(commandOutput);
-  console.log("\nCommand output data:");
-  console.log(commandOutputData);
+    // Compile command
+    let compiledCommand = `npx ${dd} ${command}`;
+    if (config) compiledCommand += ` --config ${config}`;
+    if (input) compiledCommand += ` --input ${input}`;
+    if (output) compiledCommand += ` --output ${output}`;
 
-  // Set outputs
-  core.setOutput("results", commandOutputData);
-} catch (error) {
-  core.setFailed(error.message);
+    // Run Doc Detective
+    core.info(`Running Doc Detective: ${compiledCommand}`);
+    let commandOutputData = "";
+    const options = {};   // Full options: https://github.com/actions/toolkit/blob/d9347d4ab99fd507c0b9104b2cf79fb44fcc827d/packages/exec/src/interfaces.ts#L5
+    options.listeners = {
+      stdout: (data) => {
+        commandOutputData += data.toString();
+      },
+      stdline: (data) => {
+        commandOutputData += data.toString();
+      },
+      stderr: (data) => {
+        commandOutputData += data.toString();
+      },
+      errline: (data) => {
+        commandOutputData += data.toString();
+      },
+      debug: (data) => {
+        commandOutputData += data.toString();
+      },
+    };
+    const commandOutput = await exec(compiledCommand, options);
+    console.log("Command output:");
+    console.log(commandOutput);
+    console.log("\nCommand output data:");
+    console.log(commandOutputData);
+
+    // Set outputs
+    core.setOutput("results", commandOutputData);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
-
 })();
 
 module.exports = __webpack_exports__;
